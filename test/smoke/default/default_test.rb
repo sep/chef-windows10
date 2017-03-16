78ccc82a86a05bc037c0e6930b18760c8518d79e
rmnowadly <rmnowadly@sep.com>
2017-03-16 12:33:56 -0400

    Cleaned up code based on Rubocop feedback

--- test/smoke/default/default_test.rb
+++ test/smoke/default/default_test.rb
@@ -5,15 +5,14 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-
-describe registry_key('System Policies','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System') do
+describe registry_key('System Policies', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System') do
   its('ConsentPromptBehaviorAdmin') { should eq 5 }
   its('PromptOnSecureDesktop') { should eq 1 }
   its('ConsentPromptBehaviorUser') { should eq 3 }
   its('EnableLUA') { should eq 1 }
-  its('FilterAdministratorToken') { should eq 1 } 
+  its('FilterAdministratorToken') { should eq 1 }
 end
 
-describe registry_key('System Policies UIPI','HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\UIPI') do 
-  its('Default') { should eq "0x00000001(1)" } 
+describe registry_key('System Policies UIPI', 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System\UIPI') do
+  its('Default') { should eq '0x00000001(1)' }
 end
